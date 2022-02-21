e30146784752cf3853ff279a576e149d40204350
kasual-id <azmi@kasual.id>
2022-02-21 16:36:07 +0700

    finish

--- index.php
+++ index.php
@@ -1,186 +1,161 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Schutze Test</title>
-
-    <!-- Font Style -->
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
-
-    <!-- CSS Style -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-
-    <!-- JavaScrypt -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-</head>
-<body>
-<div class="container-fluid h-100">
-    <div class="row width-empty content-1">
-        <div class="col-lg-1 col-md-12 browser-col-1">
-        </div>
-
-        <div class="col-lg-2 col-md-12 browser-col-1">
-        </div>
-
-        <div class="col-lg-2 col-md-12 browser-col-2">
-
-        </div>
-
-        <div class="col-lg-2 col-md-12 browser-col-3">
-
-        </div>
-
-        <div class="col-lg-2 col-md-12 browser-col-4">
-
-        </div>
-
-        <div class="col-lg-2 col-md-12 browser-col-5">
-
-        </div>
-
-        <div class="col-lg-1 col-md-12 browser-col-5">
-        </div>
-    </div>
-    <div class="row bg-secondary justify-content-center content-2">
-        <div class="col alert-content">
-            <h4><img class="alert-icon" src="assets/exclamation-triangle.svg" alt="warning"> Your browser <b>Internet Explorer 11</b> is deprecated</h4>
-            <p>Modern browsers protect your device from viruses and support new technologies.</p>
-            <p>That's why we decided to stop supporting your current browser or your browser version. To access our website, please download one one of the browser below.</p>
-            <button class="btn btn-secondary button-text">How do i update my browser?</button>
-        </div>
-    </div>
-    <div class="row justify-content-center content-3">
-
-        <div class="col-lg-1 col-md-12 browser-col-1">
-        </div>
-
-        <div class="col-lg-2 col-md-12 browser-col-1">
-            <div class="row">
-                <div class="col-lg-12 col-sm-3 col-3">
-                    <div class="frame">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Schutze Test</title>
+      <!-- Font Style -->
+      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+      <!-- CSS Style -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/style.css">
+      <!-- JavaScrypt -->
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+   </head>
+   <body>
+      <div class="container-fluid h-100">
+         <div class="row width-empty content-1">
+            <div class="col-lg-1 col-md-12 browser-col-1">
+            </div>
+            <div class="col-lg-2 col-md-12 browser-col-1">
+            </div>
+            <div class="col-lg-2 col-md-12 browser-col-2">
+            </div>
+            <div class="col-lg-2 col-md-12 browser-col-3">
+            </div>
+            <div class="col-lg-2 col-md-12 browser-col-4">
+            </div>
+            <div class="col-lg-2 col-md-12 browser-col-5">
+            </div>
+            <div class="col-lg-1 col-md-12 browser-col-5">
+            </div>
+         </div>
+         <div class="row bg-secondary justify-content-center content-2">
+            <div class="col alert-content">
+               <h4><img class="alert-icon" src="assets/exclamation-triangle.svg" alt="warning"> Your browser <b>Internet Explorer 11</b> is deprecated</h4>
+               <p>Modern browsers protect your device from viruses and support new technologies.</p>
+               <p>That's why we decided to stop supporting your current browser or your browser version. To access our website, please download one one of the browser below.</p>
+               <button class="btn btn-secondary button-text">How do i update my browser?</button>
+            </div>
+         </div>
+         <div class="row justify-content-center content-3">
+            <div class="col-lg-1 col-md-12 browser-col-1">
+            </div>
+            <div class="col-lg-2 col-md-12 browser-col-1">
+               <div class="row">
+                  <div class="col-lg-12 col-sm-3 col-3">
+                     <div class="frame">
                         <span class="helper"></span>
                         <img class="browser-icon" src="assets/chrome.svg" alt="">
-                    </div>
-            </div>
-            <div class="col-lg-12 col-sm-7 col-6">
-                <p class="lead"><b>GOOGLE CHROME</b></p> 
-                <p class="lead">Version 40+</p> 
-            </div>
-            <div class="col-lg-12 col-sm-2 col-3">
-                <div class="frame">
-                    <a href="#" type="button" class="btn btn-download btn-sm">
+                     </div>
+                  </div>
+                  <div class="col-lg-12 col-sm-7 col-6">
+                     <p class="lead"><b>GOOGLE CHROME</b></p>
+                     <p class="lead">Version 40+</p>
+                  </div>
+                  <div class="col-lg-12 col-sm-2 col-3">
+                     <div class="frame">
+                        <a href="#" type="button" class="btn btn-download btn-sm">
                         <img class="icon-text" src="assets/download.svg" alt="">
                         <span class="button-text">Download</span>
-                    </a>
-                </div>
+                        </a>
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
-
-    <div class="col-lg-2 col-md-12 browser-col-2">
-        <div class="row">
-            <div class="col-lg-12 col-sm-3 col-3">
-                <div class="frame">
-                    <span class="helper"></span>
-                    <img class="browser-icon" src="assets/firefox-browser.svg" alt="">
-
-                </div>
-            </div>
-            <div class="col-lg-12 col-sm-7 col-6">
-                <p class="lead"><b>MOZILLA FIREFOXE</b></p> 
-                <p class="lead">Version 40+</p> 
-            </div>
-            <div class="col-lg-12 col-sm-2 col-3">
-                <div class="frame">
-                    <a href="#" type="button" class="btn btn-download btn-sm">
+            <div class="col-lg-2 col-md-12 browser-col-2">
+               <div class="row">
+                  <div class="col-lg-12 col-sm-3 col-3">
+                     <div class="frame">
+                        <span class="helper"></span>
+                        <img class="browser-icon" src="assets/firefox-browser.svg" alt="">
+                     </div>
+                  </div>
+                  <div class="col-lg-12 col-sm-7 col-6">
+                     <p class="lead"><b>MOZILLA FIREFOXE</b></p>
+                     <p class="lead">Version 40+</p>
+                  </div>
+                  <div class="col-lg-12 col-sm-2 col-3">
+                     <div class="frame">
+                        <a href="#" type="button" class="btn btn-download btn-sm">
                         <img class="icon-text" src="assets/download.svg" alt="">
                         <span class="button-text">Download</span>
-                    </a>
-                </div>
+                        </a>
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
-
-    <div class="col-lg-2 col-md-12 browser-col-3">
-        <div class="row">
-            <div class="col-lg-12 col-sm-3 col-3">
-                <div class="frame">
-                    <span class="helper"></span>
-                    <img class="browser-icon" src="assets/edge.svg" alt="">
-
-                </div>
-            </div>
-            <div class="col-lg-12 col-sm-7 col-6">
-                <p class="lead"><b>MICROSOFT EDGE</b></p> 
-                <p class="lead">Version 40+</p> 
-            </div>
-            <div class="col-lg-12 col-sm-2 col-3">
-                <div class="frame">
-                    <a href="#" type="button" class="btn btn-download btn-sm">
+            <div class="col-lg-2 col-md-12 browser-col-3">
+               <div class="row">
+                  <div class="col-lg-12 col-sm-3 col-3">
+                     <div class="frame">
+                        <span class="helper"></span>
+                        <img class="browser-icon" src="assets/edge.svg" alt="">
+                     </div>
+                  </div>
+                  <div class="col-lg-12 col-sm-7 col-6">
+                     <p class="lead"><b>MICROSOFT EDGE</b></p>
+                     <p class="lead">Version 40+</p>
+                  </div>
+                  <div class="col-lg-12 col-sm-2 col-3">
+                     <div class="frame">
+                        <a href="#" type="button" class="btn btn-download btn-sm">
                         <img class="icon-text" src="assets/download.svg" alt="">
                         <span class="button-text">Download</span>
-                    </a>
-                </div>
+                        </a>
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
-
-    <div class="col-lg-2 col-md-12 browser-col-4">
-        <div class="row">
-            <div class="col-lg-12 col-sm-3 col-3">
-                <div class="frame">
-                    <span class="helper"></span>
-                    <img class="browser-icon" src="assets/safari.svg" alt="">
-
-                </div>
-            </div>
-            <div class="col-lg-12 col-sm-7 col-6">
-                <p class="lead"><b>APPLE SAFARI</b></p> 
-                <p class="lead">Version 40+</p> 
-            </div>
-            <div class="col-lg-12 col-sm-2 col-3">
-                <div class="frame">
-                    <a href="#" type="button" class="btn btn-download btn-sm">
+            <div class="col-lg-2 col-md-12 browser-col-4">
+               <div class="row">
+                  <div class="col-lg-12 col-sm-3 col-3">
+                     <div class="frame">
+                        <span class="helper"></span>
+                        <img class="browser-icon" src="assets/safari.svg" alt="">
+                     </div>
+                  </div>
+                  <div class="col-lg-12 col-sm-7 col-6">
+                     <p class="lead"><b>APPLE SAFARI</b></p>
+                     <p class="lead">Version 40+</p>
+                  </div>
+                  <div class="col-lg-12 col-sm-2 col-3">
+                     <div class="frame">
+                        <a href="#" type="button" class="btn btn-download btn-sm">
                         <img class="icon-text" src="assets/download.svg" alt="">
                         <span class="button-text">Download</span>
-                    </a>
-                </div>
+                        </a>
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
-
-    <div class="col-lg-2 col-md-12 browser-col-5">
-        <div class="row">
-            <div class="col-lg-12 col-sm-3 col-3">
-                <div class="frame">
-                    <span class="helper"></span>
-                    <img class="browser-icon" src="assets/opera.svg" alt="">
-
-                </div>
-            </div>
-            <div class="col-lg-12 col-sm-7 col-6">
-                <p class="lead"><b>OPERA</b></p> 
-                <p class="lead">Version 40+</p> 
-            </div>
-            <div class="col-lg-12 col-sm-2 col-3">
-                <div class="frame">
-                    <a href="#" type="button" class="btn btn-download btn-sm">
+            <div class="col-lg-2 col-md-12 browser-col-5">
+               <div class="row">
+                  <div class="col-lg-12 col-sm-3 col-3">
+                     <div class="frame">
+                        <span class="helper"></span>
+                        <img class="browser-icon" src="assets/opera.svg" alt="">
+                     </div>
+                  </div>
+                  <div class="col-lg-12 col-sm-7 col-6">
+                     <p class="lead"><b>OPERA</b></p>
+                     <p class="lead">Version 40+</p>
+                  </div>
+                  <div class="col-lg-12 col-sm-2 col-3">
+                     <div class="frame">
+                        <a href="#" type="button" class="btn btn-download btn-sm">
                         <img class="icon-text" src="assets/download.svg" alt="">
                         <span class="button-text">Download</span>
-                    </a>
-                </div>
+                        </a>
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
-
-
-    <div class="col-lg-1 col-md-12 browser-col-5">
-    </div>
-  </div>
-</div>
-</body>
+            <div class="col-lg-1 col-md-12 browser-col-5">
+            </div>
+         </div>  
+      </div>
+   </body>
 </html>
